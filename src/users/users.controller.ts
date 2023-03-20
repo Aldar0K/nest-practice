80be264ecc1ab2eafebfee9bc7fd6aa637893c6f
Aldar <aldarkonov21@gmail.com>
2023-03-20 10:25:45 +0300

    feat: add endpoints for creating and getting users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,19 @@
 import { Controller } from '@nestjs/common';
+import { Body, Get, Post } from '@nestjs/common/decorators';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  create(@Body() userDto: CreateUserDto) {
+    return this.usersService.createUser(userDto);
+  }
+
+  @Get()
+  getAll() {
+    return this.usersService.getAllUsers();
+  }
+}
