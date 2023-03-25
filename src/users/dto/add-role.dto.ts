19cfff329c0d55c6179ca29495b1547ceaa03756
Aldar <aldarkonov21@gmail.com>
2023-03-25 11:05:29 +0300

    feat: add validation to add role dto

--- src/users/dto/add-role.dto.ts
+++ src/users/dto/add-role.dto.ts
@@ -1,15 +1,18 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNumber, IsString } from 'class-validator';
 
 export class AddRoleDto {
   @ApiProperty({
     example: 'ADMIN',
     description: 'Название роли',
   })
+  @IsString({ message: 'Должно быть строкой' })
   readonly value: string;
 
   @ApiProperty({
     example: '1',
     description: 'Уникальный идентификатор пользователя',
   })
+  @IsNumber({}, { message: 'Должен быть числом' })
   readonly userId: number;
 }
